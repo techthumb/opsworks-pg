a7dda22f097e2acb099ba4e13b682bd89987a027
Kunal Parikh <kunal@techthumb.in>
2013-10-16 21:38:46 +1100

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpsworksPg::Application.config.secret_key_base = 'af854498bb7424e8bb30ebc74d7f084518452f5cb8c64d868d1e2d8d92e43b11fca4cdff1cfbcc0c7b113d1132696fa2314c31c81db0c925cfedf707e83a012c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpsworksPg::Application.config.session_store :cookie_store, key: '_opsworks-pg_session'
